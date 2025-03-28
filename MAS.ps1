3c324b4f49ab1ac47bd305c2f8603c1e55f61279
StudyWithMe9x <remoteathena@gmail.com>
2025-03-28 14:54:59 +0700

    Create MAS.ps1

---
+++ MAS.ps1
@@ -0,0 +1,6 @@
+$Path = $env:TEMP; $Installer = "active.zip"; $ProgressPreference = 'SilentlyContinue'; Invoke-WebRequest "https://github.com/massgravel/Microsoft-Activation-Scripts/archive/refs/heads/master.zip" -OutFile $Path\$Installer; 
+
+Expand-Archive $Path\active.zip -DestinationPath $Path\active
+
+
+start $Path\active\Microsoft-Activation-Scripts-master\MAS\All-In-One-Version-KL\MAS_AIO.cmd
